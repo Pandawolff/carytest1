ede6667fa8a3785796bfd901657593edcc44e699
Pandawolff <nicoterrile92@gmail.com>
2023-12-27 12:58:16 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+
+    <Html lang="en">
+      <link href="https://fonts.cdnfonts.com/css/merriweather" rel="stylesheet"></link>
+      <link href="https://fonts.cdnfonts.com/css/satoshi" rel="stylesheet"></link>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
